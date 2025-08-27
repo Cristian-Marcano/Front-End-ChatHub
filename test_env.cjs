ad41a56d5528e603965b289a4954bc86950f0005
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-08-27 16:15:00 -0400

    chore: remove accidental test script

--- test_env.cjs
+++
@@ -1,8 +0,0 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env', '@babel/preset-react']
-});
-// Need to just check what is exported
-const fs = require('fs');
-const content = fs.readFileSync('src/config/env.js', 'utf8');
-console.log("ENV file content:");
-console.log(content);
